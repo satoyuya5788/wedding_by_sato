d67127aa66b049268e80f4a5868d9f017cbbf5c9
yuya_sato123 <satoyuya5788@gmail.com>
2021-03-09 12:40:44 +0900

    管理側のリファクタリング官僚

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="py-5 bg-black">
     <div class="container">
-        <p class="m-0 text-center text-white small">Copyright &copy; sato yuya 2022</p>
+        <p class="m-0 text-center text-white small">Copyright © sato yuya 2022</p>
     </div>
 </footer>
