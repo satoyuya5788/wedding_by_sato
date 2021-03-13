04fe4a1e19aee40857e32b2af5a9e503a8335c7b
yuya_sato123 <satoyuya5788@gmail.com>
2021-03-13 23:13:40 +0900

    家族に向けての写真ページ完成１

--- resources/views/wedding/site/guide.blade.php
+++ resources/views/wedding/site/guide.blade.php
@@ -138,98 +138,40 @@
     </div>
   </section>
 
-@if(session("simple_auth")[0] === "さとうとしお" || session("simple_auth")[0] === "さとうりょうた" || session("simple_auth")[0] === "さとうけいすけ")
-<section class="mt-4 p-4 mb-3">
-    <div class="container mt-5 mb-2 bg_guide_family p-2" data-aos="fade-up" data-aos-delay="70" data-aos-duration="800" data-aos-offset="200">
-        <div class="row align-items-center text-center">
-            <div class="col-xs-12 order-lg-1 col-lg-12 pt-5 pb-4 family-msg">
-            <p class="mb-3">Dear</p>
-                <p style="font-size:1.6rem;">
-                    お父さん
-                </p>
-                <p style="font-size:1.6rem;">
-                    お母さん
-                </p>
-                <p style="font-size:1.6rem;">
-                    　　圭介兄ちゃん
-                </p>
-                <p style="font-size:1.6rem;">
-                    　　涼太兄ちゃん
-                </p>
-                <br>
-                <p style="font-size:1.3rem;" class="text-nowrap">
-                    前撮りの写真を送ります。
-                </p>
-                <p style="font-size:1.3rem;" class="text-nowrap">
-                    下からダウンロードしてね。
-                </p>
-                <div class="m-5 p-1" style="text-align: center;">
-                    <a href="/site/wedding/imagehappouen" class="btn btn-info text-nowrap">前撮りの写真へ</a>
+    @if(session("simple_auth")[0] === "さとうとしお" || session("simple_auth")[0] === "さとうりょうた" || session("simple_auth")[0] === "さとうけいすけ" || session("simple_auth")[0] === "ゆかわじゅんじ" || session("simple_auth")[0] === "うえぶせさとし")
+    <section class="mt-4 p-4 mb-3">
+        <div class="container mt-5 mb-2 bg_guide_family p-2" data-aos="fade-up" data-aos-delay="70" data-aos-duration="800" data-aos-offset="200">
+            <div class="row align-items-center text-center">
+                <div class="col-xs-12 order-lg-1 col-lg-12 pt-5 pb-4 family-msg">
+                    <p style="font-size:1.5rem;">ご家族の皆様へ</p>
+                    <img src="{{ asset('img/event_ttl_bg01.svg') }}" alt="" class="m-3" width="20">
+                    <p style="font-size:1.3rem;">
+                    前撮りの写真を
+                    </p>
+                    <p style="font-size:1.3rem;">
+                    お送り致します。
+                    </p>
+                    <br>
+                    <p style="font-size:1.3rem;">
+                    下のボタンより
+                    </p>
+                    <p style="font-size:1.3rem;">
+                    閲覧してください。
+                    </p>
+                    <div class="m-5 p-1" style="text-align: center;">
+                        <a href="/site/wedding/imagehappouen" class="btn btn-info text-nowrap">前撮りの写真へ</a>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</section>
-@elseif(session("simple_auth")[0] === "ゆかわじゅんじ")
-<section class="mt-4 p-4 mb-3">
-    <div class="container mt-5 mb-2 bg_guide_family p-2" data-aos="fade-up" data-aos-delay="70" data-aos-duration="800" data-aos-offset="200">
-        <div class="row align-items-center text-center">
-            <div class="col-xs-12 order-lg-1 col-lg-12 pt-5 pb-4 family-msg">
-                <p class="mb-3">Dear</p>
-                <p style="font-size:1.6rem;">
-                    湯川純司 様
-                </p>
-                <p style="font-size:1.6rem;">
-                    　　幸代 様
-                </p>
-                <br>
-                <p style="font-size:1.4rem;">
-                    前撮りの写真を送ります。
-                </p>
-                <div class="m-5 p-1" style="text-align: center;">
-                    <a href="/site/wedding/imagehappouen" class="btn btn-info text-nowrap">前撮りの写真へ</a>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
-@elseif(session("simple_auth")[0] === "うえぶせさとし")
-<section class="mt-4 p-4 mb-3">
-    <div class="container mt-5 mb-2 bg_guide_family p-2" data-aos="fade-up" data-aos-delay="70" data-aos-duration="800" data-aos-offset="200">
-        <div class="row align-items-center text-center">
-            <div class="col-xs-12 order-lg-1 col-lg-12 pt-5 pb-4 family-msg">
-                <p class="mb-3">Dear</p>
-                <p style="font-size:1.6rem;">
-                    上伏智志 様
-                </p>
-                <p style="font-size:1.6rem;">
-                    　　綾子 様
-                </p>
-                <p style="font-size:1.6rem;">
-                    　　智遥 様
-                </p>
-                <p style="font-size:1.6rem;">
-                    　　智葉 様
-                </p>
-                <br>
-                <p style="font-size:1.4rem;">
-                    前撮りの写真を送ります。
-                </p>
-                <div class="m-5 p-1" style="text-align: center;">
-                    <a href="/site/wedding/imagehappouen" class="btn btn-info text-nowrap">前撮りの写真へ</a>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
-@endif
+    </section>
+    @endif
 
-
-        @if(session("simple_auth"))
-            <div class="m-5 p-1" style="text-align: center;">
-                <a href="{{ route('site') }}" class="btn btn-primary text-nowrap">招待状へ戻る</a>
-            </div>
-        @endif
+    @if(session("simple_auth"))
+        <div class="m-5 p-1" style="text-align: center;">
+            <a href="{{ route('site') }}" class="btn btn-primary text-nowrap">招待状へ戻る</a>
+        </div>
+    @endif
 
   <!-- Footer -->
   @include('footer')
--- resources/views/wedding/site/non_participation.blade.php
+++ resources/views/wedding/site/non_participation.blade.php
@@ -5,10 +5,97 @@
 
 <video src="{{ asset('img/countDown_video.mp4') }}" autoplay playsinline muted loop style="opacity:0.8;"></video>
   
-    @if(session("simple_auth")[0] === "さとうとしお" || session("simple_auth")[0] === "さとうりょうた" || session("simple_auth")[0] === "さとうけいすけ" || session("simple_auth")[0] === "ゆかわじゅんじ" || session("simple_auth")[0] === "うえぶせさとし")
-        <div class="m-1 mt-5 mb-5 sticky-top p-1" style="text-align: right;">
-            <a href="/site/wedding/guidehappouen" class="btn btn-primary mr-1">案内状へ戻る</a>
+    @if(session("simple_auth")[0] === "さとうとしお" || session("simple_auth")[0] === "さとうりょうた" || session("simple_auth")[0] === "さとうけいすけ")
+    <section class="mt-4 p-4 mb-3">
+        <div class="container mt-5 mb-2 bg-dairiseki_top p-2" data-aos="fade-up" data-aos-delay="70" data-aos-duration="800" data-aos-offset="200">
+            <div class="row align-items-center text-center">
+                <div class="col-xs-12 order-lg-1 col-lg-12 pt-5 pb-4 family-msg">
+                <p class="mb-3">Dear</p>
+                    <p style="font-size:1.6rem;">
+                        お父さん
+                    </p>
+                    <p style="font-size:1.6rem;">
+                        お母さん
+                    </p>
+                    <p style="font-size:1.6rem;">
+                        　　圭介兄ちゃん
+                    </p>
+                    <p style="font-size:1.6rem;">
+                        　　涼太兄ちゃん
+                    </p>
+                    <br>
+                    <p style="font-size:1.3rem;" class="text-nowrap">
+                        前撮りの写真を送ります。
+                    </p>
+                    <p style="font-size:1.3rem;" class="text-nowrap">
+                        下からダウンロードしてね。
+                    </p>
+                </div>
+            </div>
         </div>
+    </section>
+    <div class="m-1 mt-5 mb-5 sticky-top p-1" style="text-align: right;">
+        <a href="/site/wedding/guidehappouen" class="btn btn-primary mr-1">案内状へ戻る</a>
+    </div>
+    @elseif(session("simple_auth")[0] === "ゆかわじゅんじ")
+    <section class="mt-4 p-4 mb-3">
+        <div class="container mt-5 mb-2 bg-dairiseki_top p-2" data-aos="fade-up" data-aos-delay="70" data-aos-duration="800" data-aos-offset="200">
+            <div class="row align-items-center text-center">
+                <div class="col-xs-12 order-lg-1 col-lg-12 pt-5 pb-4 family-msg">
+                <p class="mb-3">Dear</p>
+                    <p style="font-size:1.6rem;">
+                        湯川　純司　様
+                    </p>
+                    <p style="font-size:1.6rem;">
+                        　　　幸代　様
+                    </p>
+                    <br>
+                    <p style="font-size:1.3rem;" class="text-nowrap">
+                        ダウンロードしたい場合は
+                    </p>
+                    <p style="font-size:1.3rem;" class="text-nowrap">
+                        写真を長押ししてください。
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <div class="m-1 mt-5 mb-5 sticky-top p-1" style="text-align: right;">
+        <a href="/site/wedding/guidehappouen" class="btn btn-primary mr-1">案内状へ戻る</a>
+    </div>
+    @elseif(session("simple_auth")[0] === "うえぶせさとし")
+    <section class="mt-4 p-4 mb-3">
+        <div class="container mt-5 mb-2 bg-dairiseki_top p-2" data-aos="fade-up" data-aos-delay="70" data-aos-duration="800" data-aos-offset="200">
+            <div class="row align-items-center text-center">
+                <div class="col-xs-12 order-lg-1 col-lg-12 pt-5 pb-4 family-msg">
+                <p class="mb-3">Dear</p>
+                    <p style="font-size:1.6rem;">
+                        上伏　智志　様
+                    </p>
+                    <p style="font-size:1.6rem;">
+                        　　　綾子　様
+                    </p>
+                    <p style="font-size:1.6rem;">
+                        　　　智遥　様
+                    </p>
+                    <p style="font-size:1.6rem;">
+                        　　　智葉　様
+                    </p>
+                    <br>
+                    <p style="font-size:1.3rem;" class="text-nowrap">
+                        ダウンロードしたい場合は
+                    </p>
+                    <p style="font-size:1.3rem;" class="text-nowrap">
+                        写真を長押ししてください。
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <div class="m-1 mt-5 mb-5 sticky-top p-1" style="text-align: right;">
+        <a href="/site/wedding/guidehappouen" class="btn btn-primary mr-1">案内状へ戻る</a>
+    </div>
+    
     @else
     <section class="mt-3 p-4 pt-2">
         <div class="container box_msg p-5" style="box-shadow: 20px 20px 20px #ddd8d8;" data-aos="fade-down" data-aos-delay="200" data-aos-duration="3000" data-aos-offset="200" >
@@ -41,15 +128,15 @@
         <div class="m-1 mt-5 mb-5 sticky-top p-1" style="text-align: right;">
             <a href="{{ route('site') }}" class="btn btn-primary">招待状へ戻る</a>
         </div>
+        <p class="mb-2 h6 fontstyle" style="color: white; text-align:center; font-size:1.2rem;">ダウンロードしたい場合は</p>
+        <p class="h6  mt-3 fontstyle" style="color: white; text-align:center; font-size:1.2rem;">写真を長押しして下さい</p>
+        <br>
+        <div class="arrow" style="margin:0 auto">
+            <div class="down"> </div>
+        </div>
     @endif
     @endif
 
-    <p class="mb-2 h6 fontstyle" style="color: white; text-align:center; font-size:1.2rem;">ダウンロードしたい場合は</p>
-    <p class="h6  mt-3 fontstyle" style="color: white; text-align:center; font-size:1.2rem;">写真を長押しして下さい</p>
-    <br>
-    <div class="arrow" style="margin:0 auto">
-        <div class="down"> </div>
-    </div>
 
     <section class="p-2 pt-5">
         <div class="container p-5" data-aos="fade-up" data-aos-delay="70" data-aos-duration="500" data-aos-offset="200">
