17bc70cf27e0f7800741e148591561c59a209bd9
yuya_sato123 <satoyuya5788@gmail.com>
2021-03-15 11:26:17 +0900

    add brouser back

--- public/js/not_windows_back.js
+++ public/js/not_windows_back.js
@@ -1,4 +1,8 @@
-history.pushState(null, null, location.href);
-window.addEventListener('popstate', (e) => {
-  history.go(1);
-});
+$(function(){
+    history.pushState(null, null, null); //ブラウザバック無効化
+    //ブラウザバックボタン押下時
+    $(window).on("popstate", function (event) {
+      history.pushState(null, null, null);
+      window.alert('前のページに戻る場合, 「招待状へ戻る」ボタンから戻ってください。');
+    });
+   });
